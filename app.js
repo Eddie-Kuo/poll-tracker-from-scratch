db6853482f599c4544fa7eb67c6ca1a741cb02b7
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-08-02 13:52:45 -0500

    allow the users to input the question and 2 options

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 // import functions and grab DOM elements
 
+const pollForm = document.getElementById('poll-form');
+
 // let state
 
 // set event listeners 
+
+pollForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(pollForm);
+
+    const userQuestion = data.get('question');
+    const userOptionOne = data.get('input-one');
+    const userOptionTwo = data.get('input-two');
+    console.log(userQuestion, userOptionOne, userOptionTwo);
+})
+
+
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
