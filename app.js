91be963167f214804b07c45cedabd4140f8a5594
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-08-03 11:17:46 -0500

    fixed displayCurrentPoll by making a function for it

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ const optionOneDislike = document.getElementById('option-one-dislike');
 const optionTwoLike = document.getElementById('option-two-like');
 const optionTwoDislike = document.getElementById('option-two-dislike');
 
+const publishButtEl = document.getElementById('publish-button');
+
+const pastPolls = document.getElementById('past-polls');
+
 const scoreOne = document.getElementById('score-one');
 const scoreTwo = document.getElementById('score-two');
-
-
 
 // let state
 
@@ -21,6 +23,8 @@ let optionOne = '';
 let optionTwo = '';
 let optionOneCount = 0;
 let optionTwoCount = 0;
+
+let pastPollsArr = [];
 
 
 // set event listeners 
@@ -33,17 +37,81 @@ pollForm.addEventListener('submit', (e) => {
     const userQuestion = data.get('question');
     const userOptionOne = data.get('input-one');
     const userOptionTwo = data.get('input-two');
-    
-    currentQuestion.textContent = userQuestion;
-    currentOptionOne.textContent = userOptionOne;
-    currentOptionTwo.textContent = userOptionTwo;
 
+    question = userQuestion;
+    optionOne = userOptionOne;
+    optionTwo = userOptionTwo;
+
+    displayCurrentPoll();
     pollForm.reset();
+
 });
 
 
+optionOneLike.addEventListener('click', () => {
+    optionOneCount++;
+    scoreOne.textContent = optionOneCount;
+});
 
+optionOneDislike.addEventListener('click', () => {
+    optionOneCount--;
+    scoreOne.textContent = optionOneCount;
+});
+
+optionTwoLike.addEventListener('click', () => {
+    optionTwoCount++;
+    scoreTwo.textContent = optionTwoCount;
+});
+
+optionTwoDislike.addEventListener('click', () => {
+    optionTwoCount--;
+    scoreTwo.textContent = optionTwoCount;
+});
+
+publishButtEl.addEventListener('click', () => {
+    pollReset();
+
+});
+
+function pollReset() {
+    currentQuestion.textContent = '';
+    currentOptionOne.textContent = '';
+    currentOptionTwo.textContent = '';
+    scoreOne.textContent = 0;
+    scoreTwo.textContent = 0;
+}
 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+function displayCurrentPoll() {
+    currentQuestion.textContent = question;
+    currentOptionOne.textContent = optionOne;
+    currentOptionTwo.textContent = optionTwo;
+}
+    
+
+
+function displayAllPolls() { // figure out how to use these functions after fixing the issue above first
+
+}
+
+
+function renderPolls(question, optionOne, optionTwo, optionOneCount, optionTwoCount) {
+    const questionDiv = document.createElement('div');
+    const optionOneDiv = document.createElement('p');
+    const optionTwoDiv = document.createElement('p');
+    const optionOneCountDiv = document.createElement('p');
+    const optionTwoCountDiv = document.createElement('p');
+
+    questionDiv.textContent = question;
+    optionOneDiv.textContent = optionOne;
+    optionTwoDiv.textContent = optionTwo;
+    optionOneCountDiv.textContent = optionOneCount;
+    optionTwoCountDiv.textContent = optionTwoCount;
+
+    questionDiv.append(optionOneDiv, optionOneCountDiv, optionTwoDiv, optionTwoCountDiv);
+    pastPolls.append(questionDiv);
+}
+
